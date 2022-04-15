0a8bf4f5a10f7a67de67168d979c810f887db348
Isak <isak.gerre@gmail.com>
2022-04-15 21:51:37 +0200

    massa saker

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-document.getElementById("outline-path").addEventListener("animationend", () => {
-  document.getElementById("filled-line").style.opacity = "1";
+// document.getElementById("outline-path").addEventListener("animationend", () => {
+//   document.getElementById("filled-line").style.opacity = "1";
+// });
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.children[1].className += " fade";
+    }
+  });
 });
+
+setTimeout(() => {
+  document.querySelectorAll(".chapter").forEach((chapter) => {
+    observer.observe(chapter);
+  });
+}, 1000);
